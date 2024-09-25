a6ddf423d6c3b97f25a6eb710deaf00e08aab55c
eigemx <70483607+eigemx@users.noreply.github.com>
2024-09-25 21:35:19 +0300

    Update treesitter.lua: fix TSUpdate typo

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":tsupdate",
+	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
