e894f28391e66ccc6b16616258d11095e43c4f9f
eigemx <eigen.emara@gmail.com>
2024-09-25 21:22:57 +0300

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,56 @@
+vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Make line numbers default
+vim.opt.number = true
+
+-- Enable mouse mode, can be useful for resizing splits
+vim.opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim
+--  Schedile the setting after `UiEnter` because it might increase startup-time
+--  Sett `:help clipboard`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end
+)
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = false
+--vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":tsupdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+            -- auto_install is magic, it installed OpenFOAM and tmux tree sitters just by opening the files! :)
+			auto_install = true,
+			ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
+}
