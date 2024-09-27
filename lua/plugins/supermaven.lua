9874715b3a6a8fbc29cc74d3dae687173e849795
eigemx <eigen.emara@gmail.com>
2024-09-27 21:28:52 +0300

    add gitsigns

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     require("supermaven-nvim").setup({
       keymaps = {
-        accept_suggestion = "<Tab>",
+        accept_suggestion = "<C-y>",
         clear_suggestion = "<C-]>",
         accept_word = "<C-j>",
       },
-      ignore_filetypes = { cpp = false }, -- or { "cpp", }
+      ignore_filetypes = {}, -- {cpp = false }, -- or { "cpp", }
       color = {
         suggestion_color = "#686968",
         cterm = 244,
